d85a16c988be9278106c13ca19927627bf3a768e
Ericsson Schroeter <ekschro@protonmail.com>
2020-06-15 10:53:52 -0400

    Function name change.

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
 const https = require('https');
 
 // HTTPS request promisified
-const httpsRequest = (params, jsonData) => new Promise(
-  (resolve, reject) => {
-    const req = https.request(params, res => {
-      const { statusCode } = res;
-      if (statusCode < 200 || statusCode >= 300)
-        return reject(new Error(`status:${statusCode}`));
+const request = (params, jsonData) => new Promise((resolve, reject) => {
+  const req = https.request(params, res => {
+    const { statusCode } = res;
+    if (statusCode < 200 || statusCode >= 300)
+      return reject(new Error(`status:${statusCode}`));
 
-      var body = [];
-      res.on('data', c => body.push(c));
+    var body = [];
+    res.on('data', c => body.push(c));
 
-      res.on('end', () => {
-        try {
-          body = JSON.parse(Buffer.concat(body).toString());
-        } catch (e) {
-          reject(e);
-        }
-        resolve(body);
-      });
+    res.on('end', () => {
+      try {
+        body = JSON.parse(Buffer.concat(body).toString());
+      } catch (e) {
+        reject(e);
+      }
+      resolve(body);
     });
+  });
 
-    req.on('error', err => reject(err));
-    if (jsonData)
-      req.write(JSON.stringify(jsonData));
-    req.end();
-  }
-);
+  req.on('error', err => reject(err));
+  if (jsonData)
+    req.write(JSON.stringify(jsonData));
+  req.end();
+});
 
-module.exports = { httpsRequest };
+module.exports = { request };
